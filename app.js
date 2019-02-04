ef8fca107641151412f7422763420764a3425215
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2019-02-04 19:01:08 +0300

    add winston logging package

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-/* eslint-disable no-console */
-
 import express from 'express';
+import morgan from 'morgan';
+import logger from './config/logger';
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-  res.send('Hello from express\n');
+  logger.info(res.url);
+  res.send('test');
 });
 
 export default app;

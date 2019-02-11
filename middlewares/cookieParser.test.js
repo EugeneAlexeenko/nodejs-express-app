50f04a0bcec55c06b65adee14c8302ff8a838977
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2019-02-11 12:17:18 +0300

    add special symbols handling

--- middlewares/cookieParser.test.js
+++ middlewares/cookieParser.test.js
@@ -9,16 +9,9 @@ describe('cookieParser', () => {
   beforeEach(() => {
     req = {
       headers: {
-        cookie: 'COOKIE1=VALUE1;COOKIE2=VALUE2',
       },
     };
 
-    expectedReq = {
-      parsedCookies: {
-        COOKIE1: 'VALUE1',
-        COOKIE2: 'VALUE2',
-      },
-    };
 
     res = {};
 
@@ -26,27 +19,44 @@ describe('cookieParser', () => {
   });
 
   test('should populate req with parsedCookie field', () => {
-    cookieParser(req, res, nextFn);
-
-    expect(req).toHaveProperty('parsedCookies');
-    expect(req.parsedCookies).toEqual(expectedReq.parsedCookies);
-  });
-
-  test('should call next()', () => {
-    cookieParser(req, res, nextFn);
-
-    expect(nextFn).toHaveBeenCalled();
-  });
-
-  test('should not fall if no cookies provided', () => {
-    req = {
-      headers: {
-        // cookie: '',
+    req.headers.cookie = 'COOKIE1=VALUE1;COOKIE2=VALUE2';
+    expectedReq = {
+      parsedCookies: {
+        COOKIE1: 'VALUE1',
+        COOKIE2: 'VALUE2',
       },
     };
 
     cookieParser(req, res, nextFn);
 
-    expect(nextFn).toHaveBeenCalled();
+    expect(req).toHaveProperty('parsedCookies');
+    expect(req.parsedCookies).toEqual(expectedReq.parsedCookies);
+    expect(nextFn).toHaveBeenCalledWith();
+  });
+
+  test('should decode special symbols properly', () => {
+    req.headers.cookie = '%D0%BF%D0%B0%D1%80%D0%B0%D0%BC%D0%B5%D1%82%D1%801%3D%D0%B7%D0%BD%D0%B0%D1%87%D0%B5%D0%BD%D0%B8%D0%B51%3B%D0%BF%D0%B0%D1%80%D0%B0%D0%BC%D0%B5%D1%82%D1%802%3D12345';
+    expectedReq = {
+      parsedCookies: {
+        параметр1: 'значение1',
+        параметр2: '12345',
+      },
+    };
+
+    cookieParser(req, res, nextFn);
+
+    expect(req).toHaveProperty('parsedCookies');
+    expect(req.parsedCookies).toEqual(expectedReq.parsedCookies);
+    expect(nextFn).toHaveBeenCalledWith();
+  });
+
+  test('should not fall if no cookies provided', () => {
+    req = {
+      headers: {},
+    };
+
+    cookieParser(req, res, nextFn);
+
+    expect(nextFn).toHaveBeenCalledWith();
   });
 });

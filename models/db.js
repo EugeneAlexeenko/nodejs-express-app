e8d54e5d71ce02013eedc97a9828f18e32733388
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2019-02-25 14:28:54 +0300

    Change dynamic models import to explicit

--- models/db.js
+++ models/db.js
@@ -1,13 +1,10 @@
 import logger from '../config/logger';
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/sequelize.js')[env];
 
-const basename = path.basename(__filename);
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   dialect: config.dialect,
@@ -17,13 +14,9 @@ sequelize.authenticate()
   .then(() => logger.info('Connected to database'))
   .catch(error => logger.error(error.message));
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+db.User = sequelize.import('./User.js');
+db.Product = sequelize.import('./Product.js');
+db.Review = sequelize.import('./Review.js');
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {

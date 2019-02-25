2d3339cbf0b86d6155a28c9dbe20b97013cfd207
Eugene Alexeenko <eugene.alexeenko@gmail.com>
2019-02-25 12:58:26 +0300

    Remove importer functionality

--- app.js
+++ app.js
@@ -2,18 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import passport from 'passport';
-import config from './config/config.json';
-import Importer from './importer';
-import Dirwatcher from './dirwatcher';
 import cookieParser from './middlewares/cookieParser';
 import queryParser from './middlewares/queryParser';
 import routes from './routes';
 
 const app = express();
-const dirwatcher = new Dirwatcher();
-const importer = new Importer(dirwatcher); // eslint-disable-line no-unused-vars
-
-dirwatcher.watch(config.watchPath, config.watchDelay);
 
 app.use(morgan('dev'));
 app.use(cookieParser);
